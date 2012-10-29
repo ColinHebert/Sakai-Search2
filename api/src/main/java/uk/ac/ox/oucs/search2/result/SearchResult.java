d7695c27fabed0e7c2082435a2a64269c4208d81
Colin Hebert <colin.hebert@it.ox.ac.uk>
2012-10-29 15:39:02 +0000

    Add documentation for SearchResult and SearchResultList

--- api/src/main/java/uk/ac/ox/oucs/search2/result/SearchResult.java
+++ api/src/main/java/uk/ac/ox/oucs/search2/result/SearchResult.java
@@ -5,17 +5,41 @@ import uk.ac.ox.oucs.search2.content.Content;
 import java.io.Serializable;
 
 /**
+ * One entry in the results returned for a search
+ *
  * @author Colin Hebert
  */
 public interface SearchResult extends Serializable {
     Content getContent();
 
+    /**
+     * Score of the entry
+     *
+     * @return score
+     */
     double getScore();
 
+    /**
+     * Index of the entry in the entire list of results
+     *
+     * @return Current position in the list of results
+     */
     long getIndex();
 
+    /**
+     * Should the entry be displayed/used
+     * A censored entry is usually empty (see {@link CensoredSearchResult}) and shouldn't be used
+     *
+     * @return true if the result is censored, false otherwise
+     */
     boolean isCensored();
 
+    /**
+     * Short text accompanying the result
+     * Usually a relevant part of the content, containing the searched keywords
+     *
+     * @return A short text specific to the result
+     */
     String getDisplayedText();
 
     /**
--- api/src/main/java/uk/ac/ox/oucs/search2/result/SearchResultList.java
+++ api/src/main/java/uk/ac/ox/oucs/search2/result/SearchResultList.java
@@ -4,24 +4,38 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * Result of a search query, consisting in a list of {@link SearchResult}
+ *
  * @author Colin Hebert
  */
 public interface SearchResultList extends List<SearchResult> {
+    /**
+     * Get the number of entries found overall even if paging is done
+     *
+     * @return number of entries
+     */
     long getNumberResultsFound();
 
     /**
-     * Get the number of the first result in the current selection
+     * Get the position of the first result in the current selection
      *
-     * @return
+     * @return the index of the first entry of the current selection
      */
     long getStartCurrentSelection();
 
     /**
-     * Get a suggested alternative search
+     * Get a suggested search query
      *
-     * @return a suggestion for another search query, null if none if found
+     * @return a suggestion for another search query, null if none can be provided
      */
     String getSuggestion();
 
+    /**
+     * Get the term frequencies of the current results
+     * TODO: The term frequencies on a list of result is deprecated and should be removed
+     *
+     * @return term frequency for each term in the currently selected results
+     */
+    @Deprecated
     Map<String, Long> getTermFrequencies();
 }
