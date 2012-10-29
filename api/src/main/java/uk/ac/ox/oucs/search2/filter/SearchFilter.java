d6c828b53fd3cf492c7d9d15910cde859d778491
Colin Hebert <colin.hebert@it.ox.ac.uk>
2012-10-29 13:06:54 +0000

    Add documentation to the filtering system

--- api/src/main/java/uk/ac/ox/oucs/search2/filter/SearchFilter.java
+++ api/src/main/java/uk/ac/ox/oucs/search2/filter/SearchFilter.java
@@ -3,8 +3,20 @@ package uk.ac.ox.oucs.search2.filter;
 import uk.ac.ox.oucs.search2.result.SearchResult;
 
 /**
+ * Filter a {@link SearchResult} in order to change its behaviour if necessary
+ *
  * @author Colin Hebert
  */
 public interface SearchFilter {
+    /**
+     * Apply the filter on a {@link SearchResult}, and pass the result to the next filter in the chain.
+     * <p>
+     * Call {@link FilterChain#filter(SearchResult)} to continue the chain of filters
+     * </p>
+     *
+     * @param searchResult Result to filter
+     * @param filterChain  Chain of filters containing the next filters to apply
+     * @return The filtered result
+     */
     SearchResult filter(SearchResult searchResult, FilterChain filterChain);
 }
