7a0362521fec861b3c2dd748f5fb5b9669f00359
Colin Hebert <colin.hebert@it.ox.ac.uk>
2012-10-29 11:43:56 +0000

    Fix typo in the documentation

--- api/src/main/java/uk/ac/ox/oucs/search2/event/IndexEventManager.java
+++ api/src/main/java/uk/ac/ox/oucs/search2/event/IndexEventManager.java
@@ -3,7 +3,7 @@ package uk.ac.ox.oucs.search2.event;
 import org.sakaiproject.event.api.NotificationAction;
 
 /**
- * Class in charge of intercepting {@link org.sakaiproject.event.api.Event} and dispatching them to the rights
+ * Class in charge of intercepting {@link org.sakaiproject.event.api.Event} and dispatching them to the right
  * {@link IndexEventHandler}
  *
  * @author Colin Hebert
