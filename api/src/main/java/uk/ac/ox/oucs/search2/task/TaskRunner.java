e68aa2d66f31aed5c065f48721618481fdd9b9d3
Colin Hebert <hebert.colin@gmail.com>
2012-12-01 10:43:40 +0000

    Limit the usage scope of TaskRunner

--- api/src/main/java/uk/ac/ox/oucs/search2/task/TaskRunner.java
+++ api/src/main/java/uk/ac/ox/oucs/search2/task/TaskRunner.java
@@ -1,6 +1,8 @@
 package uk.ac.ox.oucs.search2.task;
 
 /**
+ * Class in charge of polling the queuing system in order to execute the {@link Task}
+ *
  * @author Colin Hebert
  */
 public interface TaskRunner {
