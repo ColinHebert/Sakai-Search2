aa41e890fa456c3838723dc26564493bd15a7cb9
Colin Hebert <hebert.colin@gmail.com>
2012-12-01 10:13:27 +0000

    Create a TaskQueueing system and a TaskRunner

---
+++ api/src/main/java/uk/ac/ox/oucs/search2/task/TaskQueueing.java
@@ -0,0 +1,8 @@
+package uk.ac.ox.oucs.search2.task;
+
+/**
+ * @author Colin Hebert
+ */
+public interface TaskQueueing {
+    void addTaskToQueue(Task task);
+}
---
+++ api/src/main/java/uk/ac/ox/oucs/search2/task/TaskRunner.java
@@ -0,0 +1,8 @@
+package uk.ac.ox.oucs.search2.task;
+
+/**
+ * @author Colin Hebert
+ */
+public interface TaskRunner {
+    void runTask(Task task);
+}
