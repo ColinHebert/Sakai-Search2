178dcc7c613d90ee821002b961c777d1c5425a59
Colin Hebert <hebert.colin@gmail.com>
2012-12-01 12:10:04 +0000

    Make sure that tasks are Serializable

--- api/src/main/java/uk/ac/ox/oucs/search2/task/Task.java
+++ api/src/main/java/uk/ac/ox/oucs/search2/task/Task.java
@@ -1,11 +1,12 @@
 package uk.ac.ox.oucs.search2.task;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
  * @author Colin Hebert
  */
-public interface Task {
+public interface Task extends Serializable {
     String getType();
     Date getCreationDate();
     String getProperty(String propertyName);
