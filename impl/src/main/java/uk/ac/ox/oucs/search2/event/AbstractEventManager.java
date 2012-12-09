f825efc3f5c8c85fe190c8408fd3fe20f48471d6
Colin Hebert <hebert.colin@gmail.com>
2012-12-09 12:24:30 +0000

    Update AbstractIndexEventManager into AbstractEventManager

--- impl/src/main/java/uk/ac/ox/oucs/search2/event/AbstractEventManager.java
+++ impl/src/main/java/uk/ac/ox/oucs/search2/event/AbstractEventManager.java
@@ -6,31 +6,38 @@ import org.slf4j.LoggerFactory;
 import org.w3c.dom.Element;
 
 /**
+ * Abstract implementation of {@link EventManager}, handles the registration to events and their interception.
+ * <p>
+ * This class removes the complexity of {@link NotificationAction} and automatically registers itself for relevant events.
+ * </p>
+ *
  * @author Colin Hebert
  */
-public abstract class AbstractIndexEventManager implements IndexEventManager {
-    private static final Logger logger = LoggerFactory.getLogger(AbstractIndexEventManager.class);
+public abstract class AbstractEventManager implements EventManager {
+    private static final Logger logger = LoggerFactory.getLogger(AbstractEventManager.class);
     private NotificationEdit notification;
 
-    public AbstractIndexEventManager(NotificationService notificationService) {
+    public AbstractEventManager(NotificationService notificationService) {
         notification = notificationService.addTransientNotification();
         // set the filter to any site related resource
         notification.setResourceFilter("/");
         notification.setAction(this);
-        logger.info("The IndexEventManager has now a notification for relevant events");
+        logger.info("The EventManager has now a notification for relevant events");
     }
 
     @Override
-    public void addIndexEventHandler(IndexEventHandler indexEventHandler) {
-        for (String eventName : indexEventHandler.getSupportedEventTypes()) {
+    public void addEventHandler(EventHandler eventHandler) {
+        logger.info("Adding '" + eventHandler + "' event types to the list of monitored ones.");
+        for (String eventName : eventHandler.getSupportedEventTypes()) {
             notification.addFunction(eventName);
-            logger.info("The event '" + eventName + "' is now monitored by the index event manager.");
+            logger.info("The event '" + eventName + "' is now monitored by the event manager.");
         }
     }
 
     @Override
     public final void notify(Notification notification, Event event) {
-        logger.debug("The event '" + event.getResource() + "' has been caught.");
+        if (logger.isDebugEnabled())
+            logger.debug("The event '" + event + "' has been caught.");
         try {
             notify(event);
         } catch (Exception e) {
@@ -38,10 +45,15 @@ public abstract class AbstractIndexEventManager implements IndexEventManager {
         }
     }
 
+    /**
+     * Notifies the system of the interception of an {@link Event}.
+     *
+     * @param event intercepted event
+     */
     protected abstract void notify(Event event);
 
     //-------------------------------------------------------------------------------
-    //  The following methods aren't relevant for the IndexEventManager
+    //  The following methods aren't relevant for the EventManager
     //-------------------------------------------------------------------------------
 
     @Override
