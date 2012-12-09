6f8c612cec49e50b1717c57fcde0bffdcbd10ed2
Colin Hebert <hebert.colin@gmail.com>
2012-12-09 11:57:47 +0000

    Add contentType and contentName to StreamDocument

--- api/src/main/java/uk/ac/ox/oucs/search2/document/StreamDocument.java
+++ api/src/main/java/uk/ac/ox/oucs/search2/document/StreamDocument.java
@@ -3,7 +3,11 @@ package uk.ac.ox.oucs.search2.document;
 import java.io.InputStream;
 
 /**
- * Document for which the content is accessible through an {@link InputStream}
+ * Document for which the content is accessible through an {@link InputStream}.
+ * <p>
+ * A StreamDocument also provides optional methods for content type detection to help the content parser to determine
+ * how the content should be interpreted.
+ * </p>
  *
  * @author Colin Hebert
  */
@@ -12,4 +16,21 @@ public interface StreamDocument extends Document {
      * Gets the document's content
      */
     InputStream getContent();
+
+    /**
+     * Gets the name of the streamed content (optional).
+     * <p>
+     * The name should be similar to a file name; the extension may help to determine the content type.
+     * </p>
+     *
+     * @return a file name for the streamed content, or null if not provided.
+     */
+    String getContentName();
+
+    /**
+     * Gets the mime type of the streamed content (optional).
+     *
+     * @return the mime type of the streamed content, or null if not provided.
+     */
+    String getContentType();
 }
