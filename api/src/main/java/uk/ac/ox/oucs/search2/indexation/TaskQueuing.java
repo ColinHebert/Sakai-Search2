2efba461c209ce7bd14e73eb439b840776cffe29
Colin Hebert <hebert.colin@gmail.com>
2012-12-09 11:09:23 +0000

    Move TaskQueuing to the indexation package

---
+++ api/src/main/java/uk/ac/ox/oucs/search2/indexation/TaskQueuing.java
@@ -0,0 +1,18 @@
+package uk.ac.ox.oucs.search2.indexation;
+
+/**
+ * Queuing system allowing Tasks execution to be deferred.
+ * <p>
+ * Each queuing system also provides a {@link TaskRunner} implementation which will be in charge of handling the next {@link Task}.
+ * </p>
+ *
+ * @author Colin Hebert
+ */
+public interface TaskQueuing {
+    /**
+     * Adds the Task to the queuing system.
+     *
+     * @param task task to queue.
+     */
+    void addTaskToQueue(Task task);
+}
--- api/src/main/java/uk/ac/ox/oucs/search2/task/TaskQueuing.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.ox.oucs.search2.task;
-
-/**
- * @author Colin Hebert
- */
-public interface TaskQueuing {
-    void addTaskToQueue(Task task);
-}
