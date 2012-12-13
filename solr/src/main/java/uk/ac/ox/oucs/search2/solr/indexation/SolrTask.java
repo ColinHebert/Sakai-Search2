3a96268f471ada0cceca8ea0c23a6045023c685c
Colin Hebert <colin.hebert@it.ox.ac.uk>
2012-12-13 14:01:14 +0000

    Move SolrTask to the indexation package

---
+++ solr/src/main/java/uk/ac/ox/oucs/search2/solr/indexation/SolrTask.java
@@ -0,0 +1,55 @@
+package uk.ac.ox.oucs.search2.solr.indexation;
+
+import org.joda.time.DateTime;
+import uk.ac.ox.oucs.search2.indexation.DefaultTask;
+
+/**
+ * Task specific to a Solr index.
+ * <p>
+ * Additional Tasks supported by Solr servers such as commit and optimise.
+ * </p>
+ *
+ * @author Colin Hebert
+ */
+public class SolrTask extends DefaultTask {
+
+    /**
+     * Creates a {@code Task} specific for a Solr server.
+     * <p>
+     * The creation date of the task will be set automatically to the current DateTime.
+     * </p>
+     *
+     * @param type type of task
+     */
+    public SolrTask(Type type) {
+        super(type.getTypeName(), DateTime.now());
+    }
+
+    /**
+     * Creates a {@code Task} specific for a Solr server.
+     *
+     * @param type         type of task
+     * @param creationDate the creation date of the task.
+     */
+    public SolrTask(Type type, DateTime creationDate) {
+        super(type.getTypeName(), creationDate);
+    }
+
+    /**
+     * Task types specific to Solr
+     */
+    public static enum Type {
+        /**
+         * Optimisation of the index.
+         */
+        OPTIMISE,
+        /**
+         * Commit.
+         */
+        COMMIT;
+
+        public String getTypeName() {
+            return SolrTask.class.getCanonicalName() + "." + name();
+        }
+    }
+}
