12c55fd51403cf5b1b478c9b1a167b238b1fa642
Colin Hebert <colin.hebert@it.ox.ac.uk>
2012-12-14 14:58:15 +0000

    Update SolrSearchResult to implement SearchResult directly

--- solr/src/main/java/uk/ac/ox/oucs/search2/solr/result/SolrSearchResult.java
+++ solr/src/main/java/uk/ac/ox/oucs/search2/solr/result/SolrSearchResult.java
@@ -1,27 +1,32 @@
 package uk.ac.ox.oucs.search2.solr.result;
 
-import uk.ac.ox.oucs.search2.content.Content;
-import uk.ac.ox.oucs.search2.result.AbstractSearchResult;
+import uk.ac.ox.oucs.search2.document.Document;
+import uk.ac.ox.oucs.search2.result.SearchResult;
 
 /**
  * @author Colin Hebert
  */
-public class SolrSearchResult extends AbstractSearchResult {
-    private final Content content;
+public class SolrSearchResult implements SearchResult {
+    private final Document document;
     private final double score;
     private final long index;
     private final String displayedText;
 
-    public SolrSearchResult(Content content, double score, long index, String displayedText) {
-        this.content = content;
+    public SolrSearchResult(Document document, double score, long index, String displayedText) {
+        this.document = document;
         this.score = score;
         this.index = index;
         this.displayedText = displayedText;
     }
 
     @Override
-    public Content getContent() {
-        return content;
+    public Document getDocument() {
+        return document;
+    }
+
+    @Override
+    public boolean isCensored() {
+        return false;
     }
 
     @Override
