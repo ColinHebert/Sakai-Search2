3c55f6fe3130bb7bf3e27414c9240499e4dfb0b1
Colin Hebert <colin.hebert@it.ox.ac.uk>
2012-12-18 11:23:16 +0000

    Create the event handler for Index events

---
+++ impl/src/main/java/uk/ac/ox/oucs/search2/event/IndexEventHandler.java
@@ -0,0 +1,47 @@
+package uk.ac.ox.oucs.search2.event;
+
+import org.joda.time.DateTime;
+import org.sakaiproject.event.api.Event;
+import uk.ac.ox.oucs.search2.indexation.DefaultTask;
+import uk.ac.ox.oucs.search2.indexation.Task;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Captures and handles events linked the index.
+ *
+ * @author Colin Hebert
+ */
+public class IndexEventHandler implements EventHandler {
+    public static final String INDEX_SITE_EVENT = IndexEventHandler.class.getCanonicalName() + ".indexSiteEvent";
+    public static final String UNINDEX_SITE_EVENT = IndexEventHandler.class.getCanonicalName() + ".unindexSiteEvent";
+    public static final String INDEX_ALL_EVENT = IndexEventHandler.class.getCanonicalName() + ".indexAllEvent";
+    public static final String UNINDEX_ALL_EVENT = IndexEventHandler.class.getCanonicalName() + ".unindexAllEvent";
+    private static final Collection<String> HANDLED_EVENTS = Arrays.asList(INDEX_SITE_EVENT, UNINDEX_SITE_EVENT, INDEX_ALL_EVENT, UNINDEX_ALL_EVENT);
+
+    @Override
+    public Collection<String> getSupportedEventTypes() {
+        return HANDLED_EVENTS;
+    }
+
+    @Override
+    public Task getTask(Event event) {
+        DateTime creationDate = new DateTime(event.getEventTime());
+        if (INDEX_SITE_EVENT.equals(event.getEvent()))
+            return new DefaultTask(DefaultTask.Type.INDEX_SITE, creationDate).setProperty(DefaultTask.SITE_ID, event.getResource());
+        else if (UNINDEX_SITE_EVENT.equals(event.getEvent()))
+            return new DefaultTask(DefaultTask.Type.UNINDEX_SITE, creationDate).setProperty(DefaultTask.SITE_ID, event.getResource());
+        else if (INDEX_ALL_EVENT.equals(event.getEvent()))
+            return new DefaultTask(DefaultTask.Type.INDEX_ALL, creationDate);
+        else if (UNINDEX_ALL_EVENT.equals(event.getEvent()))
+            return new DefaultTask(DefaultTask.Type.UNINDEX_ALL, creationDate);
+        else
+            return new DefaultTask(DefaultTask.Type.IGNORE);
+    }
+
+    @Override
+    public boolean isHandled(Event event) {
+        return getSupportedEventTypes().contains(event.getEvent());
+    }
+}
